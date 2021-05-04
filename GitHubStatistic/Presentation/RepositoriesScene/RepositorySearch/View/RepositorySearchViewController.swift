ffe8d75ef929a2e97bedac7760061daaedd4a257
Jaime Alcántara <jaime061197@gmail.com>
2021-05-04 16:36:53 +0100

    Add search button

--- GitHubStatistic/Presentation/RepositoriesScene/RepositorySearch/View/RepositorySearchViewController.swift
+++ GitHubStatistic/Presentation/RepositoriesScene/RepositorySearch/View/RepositorySearchViewController.swift
@@ -14,6 +14,8 @@ class RepositorySearchViewController: BaseViewController {
 
     @IBOutlet weak var searchView: UIView!
     
+    @IBOutlet weak var newSearchView: UIView!
+    
     @IBOutlet weak var searchCollectionView: UICollectionView!
     
     @IBOutlet weak var searchViewTopToViewCenterConstraint: NSLayoutConstraint!
@@ -21,6 +23,10 @@ class RepositorySearchViewController: BaseViewController {
     @IBOutlet weak var searchViewTopToViewTopConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var noPreviousSearchLabel: UILabel!
+    
+    @IBOutlet weak var searchButton: UIButton!
+    
+    @IBOutlet weak var searchButtonBottomToViewBottomConstraint: NSLayoutConstraint!
     
     var viewModel: RepositorySearchViewModel!
     
@@ -31,26 +37,48 @@ class RepositorySearchViewController: BaseViewController {
 
     private func setupBindings() {
         
+        self.searchCollectionView.rx
+            .setDelegate(self)
+            .disposed(by:self.disposeBag)
+        
+        self.searchButton.rx.tap
+            .bind(to: self.viewModel.searchButtonTap)
+            .disposed(by: self.disposeBag)
+        
         self.searchView.rx.swipeGesture([.up, .down]).when(.ended).map { gesture in
             gesture.direction == .up
         }.asObservable()
-        .bind(to: self.viewModel.searchViewGesture)
+        .bind(to: self.viewModel.searchViewSwipeGesture)
         .disposed(by: self.disposeBag)
         
         self.viewModel.searchViewIsUp.drive { isViewUp in
             self.changeSearchViewPosition(up: isViewUp)
         }.disposed(by: self.disposeBag)
         
-        self.searchCollectionView.rx
-            .setDelegate(self)
-            .disposed(by:self.disposeBag)
+        self.viewModel.showSearchButton.drive { showSearchButton in
+            self.showSearchButton(visibility: showSearchButton)
+        }.disposed(by: self.disposeBag)
+        
+        self.viewModel.hidePreviousSearchInterface.drive { hideSearchView in
+            self.changeSearchViewVisibility(visibility: !hideSearchView)
+        }.disposed(by: self.disposeBag)
+        
+        self.viewModel.showNewSearchInterface.drive { showNewSearchView in
+            self.changeNewSearchViewVisibility(visibility: showNewSearchView)
+        }.disposed(by: self.disposeBag)
         
         self.viewModel.previousSearchCells.drive(self.searchCollectionView.rx.items(cellIdentifier: "RecentRepositoryCell", cellType: RecentRepositoryCell.self)) { i, cellModel, cell in
             cell.repositoryName.text = cellModel.name
             cell.numberOfCommits.text = String(cellModel.numberOfCommits)
         }.disposed(by: self.disposeBag)
 
-        
+    }
+    
+    private func showSearchButton(visibility:Bool) {
+        self.searchButtonBottomToViewBottomConstraint.constant = visibility ? 50 : -200
+        UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseIn) {
+            self.view.layoutIfNeeded()
+        }
     }
     
     private func changeSearchViewPosition(up:Bool) {
@@ -67,7 +95,17 @@ class RepositorySearchViewController: BaseViewController {
         }
     }
     
+    private func changeSearchViewVisibility(visibility:Bool) {
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveLinear) {
+            self.searchView.alpha = visibility ? 1.0 : 0.0
+        }
+    }
     
+    private func changeNewSearchViewVisibility(visibility:Bool) {
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveLinear) {
+            self.newSearchView.alpha = visibility ? 1.0 : 0.0
+        }
+    }
     
 }
 
--- GitHubStatistic/Presentation/RepositoriesScene/RepositorySearch/ViewModel/RepositorySearchViewModel.swift
+++ GitHubStatistic/Presentation/RepositoriesScene/RepositorySearch/ViewModel/RepositorySearchViewModel.swift
@@ -16,18 +16,26 @@ class RepositorySearchViewModel : BaseViewModel {
     
     // MARK: Input
     let searchText : Driver<String>! = nil
-    let searchButton : Driver<Void>! = nil
     let isUserSearch : Driver<Bool>! = nil
-    let searchViewGesture : PublishSubject<Bool>! = PublishSubject<Bool>()
+    let searchViewSwipeGesture : PublishSubject<Bool>! = PublishSubject<Bool>()
+    let searchButtonTap : PublishSubject<Void>! = PublishSubject<Void>()
     
     // MARK: Output
-    let searchViewIsUp : Driver<Bool>!
-    let previousSearchCells: Driver<[Repository]>
+    let searchViewIsUp : Driver<Bool>
+    let previousSearchCells : Driver<[Repository]>
+    let showSearchButton : Driver<Bool>
+    let hidePreviousSearchInterface : Driver<Bool>
+    let showNewSearchInterface : Driver<Bool>
     
     init(getRecentRepositoriesUseCase : GetRecentRepositoriesUseCase) {
         self.getRecentRepositoriesUseCase = getRecentRepositoriesUseCase
-        self.searchViewIsUp = self.searchViewGesture.asDriver(onErrorJustReturn: false)
+        self.searchViewIsUp = self.searchViewSwipeGesture.asDriver(onErrorJustReturn: false)
         self.previousSearchCells = self.getRecentRepositoriesUseCase.execute().asDriver(onErrorJustReturn: [Repository]())
+        self.showSearchButton = self.searchViewSwipeGesture.asDriver(onErrorJustReturn: false)
+        
+        let searchButtonTappedSubscription = self.searchButtonTap.throttle(.milliseconds(500), latest: false, scheduler: MainScheduler.instance).share()
+        self.hidePreviousSearchInterface = searchButtonTappedSubscription.map{true}.asDriver(onErrorJustReturn: false)
+        self.showNewSearchInterface = searchButtonTappedSubscription.delay(.milliseconds(300), scheduler: MainScheduler.instance).map{true}.asDriver(onErrorJustReturn: false)
     }
     
     
